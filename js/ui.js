d3c3ab51ea1a056b4367c9ee7622bed381c21ae4
kirill120095 <49593461+kirill120095@users.noreply.github.com>
2026-06-29 11:45:03 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -26,6 +26,10 @@ export function initUI() {
         lockToggleMain: document.getElementById('lockToggleMain')
     });
 
+    // --- Скрываем кнопки настроек и статистики до входа ---
+    document.getElementById('statsToggleBtn').style.display = 'none';
+    document.getElementById('settingsBtn').style.display = 'none';
+
     // --- Вкладки авторизации ---
     const tabLogin = document.getElementById('tabLogin');
     const tabRegister = document.getElementById('tabRegister');
